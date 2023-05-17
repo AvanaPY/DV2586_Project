d921f6f14f851284bc115ec523c62eb6c3f6c9fd
ubk8751 <ubk8751@gmail.com>
2023-05-17 21:54:44 +0200

    Squeezified the code

--- data/concat.py
+++ data/concat.py
@@ -7,52 +7,34 @@ import emnist
 from typing import *
 
 def _local_get_img():
-    x, y = emnist.extract_training_samples('balanced')
-    x = x[0:50]
-    y = y[0:50]
-    xt = []
-    yt = []
+    x, y = [lst[0:50] for lst in emnist.extract_training_samples('balanced')]
+    xt, yt = [[] for _ in range(2)]
     t = -1
-    for i, item in enumerate(x):
+    for i in range(len(x)):
         if i % 10 == 0:
-            xt.append([item])
+            xt.append([x[i]])
+            yt.append([y[i]])
             t += 1
         else:
-            xt[t].append(item)
-    t = -1
-    for i, item in enumerate(y):
-        if i % 10 == 0:
-            yt.append([item])
-            t += 1
-        else:
-            yt[t].append(item)
+            xt[t].append(x[i])
+            yt[t].append(y[i])
     return xt, yt
 
 def horizontal(lst:list[np.ndarray]):
     full_row = 0
     for i, item in enumerate(lst):
-        if i == 0:
-            full_row = item
-        else:
-            full_row = np.concatenate((full_row, item), axis=1)
+        full_row = item if i == 0 else np.concatenate((full_row, item), axis=1)
     return full_row
 
 def vertical(lst:list[list[np.ndarray]]):
     space = np.array([[0 for i in range(28)] for i in range(28)])
-    len_lst = [len(x) for x in lst]
+    lrg = max([len(x) for x in lst])
     finale = 0
     for i, item in enumerate(lst):
-        if len(item) < max(len_lst):
-            for j in range(max(len_lst) - len(item)):
+        if len(item) < lrg:
+            for j in range(lrg - len(item)):
                 item.append(space)
-        row = horizontal(item)
-        if i == 0:
-            finale = row
-            print(f'{i}, {len(finale)}')
-        else:
-            finale = np.concatenate((finale, row), axis=0)
-            print(f'{i}, {len(finale)}')
-    print(f'Final, {len(finale)}')
+        finale = horizontal(item) if i == 0 else np.concatenate((finale, horizontal(item)), axis=0)
     return finale
 
 # Test section
@@ -60,6 +42,5 @@ if __name__ == "__main__":
     x, y = _local_get_img()
     del x[4][1]
     concat = vertical(x)
-    print(concat.shape)
     plt.imshow(concat, interpolation="nearest", cmap="gray")
     plt.show()
