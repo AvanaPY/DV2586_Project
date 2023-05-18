2d851e74d1b48904b6cd7ae9e456b0ac295c1d88
ubk8751 <ubk8751@gmail.com>
2023-05-18 21:50:03 +0200

    Restructuring

--- otils/otils.py
+++ otils/otils.py
@@ -6,6 +6,8 @@ import emnist
 # Meh functions
 from typing import *
 
+space = np.array([[0 for i in range(28)] for i in range(28)])
+
 def _local_get_img():
     x, y = [lst[0:50] for lst in emnist.extract_training_samples('balanced')]
     xt, yt = [[] for _ in range(2)]
@@ -27,7 +29,6 @@ def horizontal(lst:list[np.ndarray]):
     return full_row
 
 def vertical(lst:list[list[np.ndarray]]):
-    space = np.array([[0 for i in range(28)] for i in range(28)])
     lrg = max([len(x) for x in lst])
     finale = 0
     for i, item in enumerate(lst):
@@ -37,9 +38,27 @@ def vertical(lst:list[list[np.ndarray]]):
         finale = horizontal(item) if i == 0 else np.concatenate((finale, horizontal(item)), axis=0)
     return finale
 
+"""
+Height and width should be in number of characters you wish to have in the image
+"""
+def padding(img:np.ndarray, height:int, width:int):
+    if img.shape[0]/28 > height:
+        print("Image's height is already larger than desired height.")
+        return img 
+    if img.shape[1]/28 > width:
+        print("Image's width is already larger than desired width.")
+        return img
+    
+    if img.shape[0]/28 % 2 == 0:
+        pass
+    else:
+        pass
+    return img
+
 # Test section
 if __name__ == "__main__":
     x, y = _local_get_img()
     concat = vertical(x)
+    print(concat.shape)
     plt.imshow(concat, interpolation="nearest", cmap="gray")
     plt.show()
